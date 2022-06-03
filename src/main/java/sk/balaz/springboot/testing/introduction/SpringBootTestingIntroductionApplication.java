7d9ada98bf0390aa41a766e6ee0266d2f8fea6d0
Tomas Balaz <tomasba@amdocs.com>
2022-06-03 13:52:35 +0200

    Created spring-boot-testing-introduction application

---
+++ src/main/java/sk/balaz/springboot/testing/introduction/SpringBootTestingIntroductionApplication.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboot.testing.introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestingIntroductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/balaz/springboot/testing/introduction/SpringBootTestingIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboot.testing.introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingIntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
