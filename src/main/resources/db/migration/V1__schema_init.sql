9e3f05e030a977abcbe022ccf8d60383bc04e0c2
Tomas Balaz <tomasba@amdocs.com>
2022-06-06 08:32:13 +0200

    Created V1__schema_init.sql flyway script

--- src/main/resources/db/migration/V1__schema_init.sql
+++ src/main/resources/db/migration/V1__schema_init.sql
@@ -1,1 +1,20 @@
 create sequence customer_sequence start with 1 increment by 50;
+create sequence product_sequence start with 1 increment by 50;
+
+create table customers
+(
+    id    bigint default nextval('customer_sequence') not null,
+    email varchar(255)                                not null,
+    name  varchar(255)                                not null,
+    primary key (id)
+);
+
+create table products
+(
+    id          bigint default nextval('product_sequence') not null,
+    name        varchar(255)                               not null,
+    description varchar(255),
+    price       numeric                                    not null,
+    disabled    boolean default false,
+    primary key (id)
+);
