1351b5dbdd22ca11f9a712843e49730311b63c91
Tomas Balaz <balaztomas0@gmail.com>
2022-07-10 14:09:46 +0200

    Run ProductRepositoryTest with test containers

--- src/test/java/sk/balaz/springboot/testing/introduction/product/ProductRepositoryTest.java
+++ src/test/java/sk/balaz/springboot/testing/introduction/product/ProductRepositoryTest.java
@@ -1,8 +1,15 @@
 package sk.balaz.springboot.testing.introduction.product;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
@@ -11,12 +18,34 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ProductRepositoryTest {
+
+    private static PostgreSQLContainer postgreSQLContainer =
+            new PostgreSQLContainer(DockerImageName.parse("postgres:14-alpine"));
 
     @Autowired
     private ProductRepository productRepository;
     @Autowired
     private EntityManager entityManager;
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url",postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username",postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password",postgreSQLContainer::getPassword);
+
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        postgreSQLContainer.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        postgreSQLContainer.stop();
+    }
 
     @Test
     void shouldGetAllActiveProducts() {
