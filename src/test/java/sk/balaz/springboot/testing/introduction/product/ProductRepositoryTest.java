24348065a7afa639a6611480427076e0be6b8dee
Tomas Balaz <balaztomas0@gmail.com>
2022-07-11 12:39:36 +0200

    How to use test containers jdbc url.

--- src/test/java/sk/balaz/springboot/testing/introduction/product/ProductRepositoryTest.java
+++ src/test/java/sk/balaz/springboot/testing/introduction/product/ProductRepositoryTest.java
@@ -1,18 +1,10 @@
 package sk.balaz.springboot.testing.introduction.product;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
+import org.springframework.test.context.TestPropertySource;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
@@ -22,40 +14,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///databasename"
+})
 class ProductRepositoryTest {
-
-    static GenericContainer sqlContainer =
-            new GenericContainer(DockerImageName.parse("postgres:14-alpine"));
-
-    @Container
-    static PostgreSQLContainer<?> postgresqlContainer =
-            new PostgreSQLContainer<>(DockerImageName.parse("postgres:14-alpine"))
-                    .withDatabaseName("test")
-                    .withUsername("admin")
-                    .withPassword("secret");
 
     @Autowired
     private ProductRepository productRepository;
     @Autowired
     private EntityManager entityManager;
-
-    @DynamicPropertySource
-    static void overrideProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresqlContainer::getUsername);
-        registry.add("spring.datasource.password", postgresqlContainer::getPassword);
-    }
-
-//    @BeforeAll
-//    static void beforeAll() {
-//        postgresqlContainer.start();
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        postgresqlContainer.stop();
-//    }
 
     @Test
     void shouldGetAllActiveProducts() {
