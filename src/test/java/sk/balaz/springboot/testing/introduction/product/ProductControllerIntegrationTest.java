4693b945d69377bf223c98a8a333684fbde1ed72
Tomas Balaz <balaztomas0@gmail.com>
2022-07-11 12:54:58 +0200

    How to run ProductControllerIntegrationTest in test container

--- src/test/java/sk/balaz/springboot/testing/introduction/product/ProductControllerIntegrationTest.java
+++ src/test/java/sk/balaz/springboot/testing/introduction/product/ProductControllerIntegrationTest.java
@@ -7,10 +7,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Testcontainers
 class ProductControllerIntegrationTest {
+
+    static GenericContainer sqlContainer =
+            new GenericContainer(DockerImageName.parse("postgres:14-alpine"));
+
+    @Container
+    static PostgreSQLContainer<?> postgresqlContainer =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:14-alpine"))
+                    .withDatabaseName("test")
+                    .withUsername("admin")
+                    .withPassword("secret");
 
     @Autowired
     private MockMvc mockMvc;
